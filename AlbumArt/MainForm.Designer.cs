c90242c44de71914e3925daa535863719301017b
DESKTOP-1GU391B\Malcolm MacDonald <mac97@shaw.ca>
2018-10-26 01:10:45 -0700

    Init commit

---
+++ AlbumArt/MainForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace AlbumArt
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.mainTextBox = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.folderSelectButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.retrieveButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageSelector = new System.Windows.Forms.NumericUpDown();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageSelector)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.11126F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 81.88874F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.mainTextBox, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(770, 563);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // mainTextBox
+            // 
+            this.mainTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTextBox.Location = new System.Drawing.Point(142, 481);
+            this.mainTextBox.Multiline = true;
+            this.mainTextBox.Name = "mainTextBox";
+            this.mainTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.mainTextBox.Size = new System.Drawing.Size(625, 79);
+            this.mainTextBox.TabIndex = 0;
+            this.mainTextBox.TextChanged += new System.EventHandler(this.mainTextBox_TextChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.folderSelectButton);
+            this.flowLayoutPanel1.Controls.Add(this.connectButton);
+            this.flowLayoutPanel1.Controls.Add(this.retrieveButton);
+            this.flowLayoutPanel1.Controls.Add(this.imageSelector);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(133, 354);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // folderSelectButton
+            // 
+            this.folderSelectButton.AutoSize = true;
+            this.folderSelectButton.Location = new System.Drawing.Point(3, 3);
+            this.folderSelectButton.Name = "folderSelectButton";
+            this.folderSelectButton.Size = new System.Drawing.Size(79, 23);
+            this.folderSelectButton.TabIndex = 0;
+            this.folderSelectButton.Text = "Select Folder";
+            this.folderSelectButton.UseVisualStyleBackColor = true;
+            this.folderSelectButton.Click += new System.EventHandler(this.folderSelectButton_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(770, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Tag = "";
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadFolderToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadFolderToolStripMenuItem
+            // 
+            this.loadFolderToolStripMenuItem.Name = "loadFolderToolStripMenuItem";
+            this.loadFolderToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.loadFolderToolStripMenuItem.Text = "Load Folder";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // connectButton
+            // 
+            this.connectButton.AutoSize = true;
+            this.connectButton.Location = new System.Drawing.Point(3, 32);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(104, 23);
+            this.connectButton.TabIndex = 1;
+            this.connectButton.Text = "Connect to Spotify";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // retrieveButton
+            // 
+            this.retrieveButton.AutoSize = true;
+            this.retrieveButton.Location = new System.Drawing.Point(3, 61);
+            this.retrieveButton.Name = "retrieveButton";
+            this.retrieveButton.Size = new System.Drawing.Size(80, 23);
+            this.retrieveButton.TabIndex = 2;
+            this.retrieveButton.Text = "Get album art";
+            this.retrieveButton.UseVisualStyleBackColor = true;
+            this.retrieveButton.Visible = false;
+            this.retrieveButton.Click += new System.EventHandler(this.retrieveButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(142, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(625, 472);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // imageSelector
+            // 
+            this.imageSelector.Location = new System.Drawing.Point(3, 90);
+            this.imageSelector.Name = "imageSelector";
+            this.imageSelector.Size = new System.Drawing.Size(120, 20);
+            this.imageSelector.TabIndex = 4;
+            this.imageSelector.Visible = false;
+            this.imageSelector.ValueChanged += new System.EventHandler(this.imageSelector_ValueChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 587);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Album Art";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageSelector)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox mainTextBox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button folderSelectButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button retrieveButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown imageSelector;
+    }
+}
+
---
+++ AlbumArt/MainForm.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Tesseract;
+
+namespace AlbumArt
+{
+    public partial class MainForm : Form
+    {
+        ArtRetrieval artRetiever;
+        string currentSaveFolder;
+        Image currentImage;
+        string[] imageFiles;
+        
+        Rect[] currentRects;
+        
+        public MainForm()
+        {
+
+            InitializeComponent();
+           currentRects = new Rect[0];
+            pictureBox1.Paint += PictureBox1_Paint;
+            artRetiever = new ArtRetrieval(this);
+            if (Properties.Settings.Default.FolderPath != "")
+            {
+                currentSaveFolder = Properties.Settings.Default.FolderPath;
+                PrintString("Folder found: " + currentSaveFolder);
+
+            }
+            
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen outlinePen = new Pen(new System.Drawing.SolidBrush(Color.Red));
+            Brush fillBrush = new System.Drawing.SolidBrush(Color.FromArgb(200,255,0,0));
+
+            for (int i = 0; i < currentRects.Length; i++)
+            {
+                if(currentRects[i] != null)
+                {
+                     Rect currentRect = currentRects[i];
+                    //Size sizestep1 = Size.Subtract(new Size(pictureBox1.Image.Size.Width / 2, pictureBox1.Image.Size.Height / 2), pictureBox1.Size);
+
+                    // Convert to point.
+                    //Point BottomRightCoords = new Point(sizestep1.Width, sizestep1.Height);
+
+                    //float xScale = (float)pictureBox1.Width / currentImage.Width;
+                    int xPos = currentRect.X1;
+                    int yPos = currentRect.Y1;
+                    int rectWidth = currentRect.X2 - currentRect.X1;
+                    int rectHeight = currentRect.Y2 - currentRect.Y1;
+
+                    e.Graphics.DrawRectangle(outlinePen, xPos,yPos, rectWidth, rectHeight);
+                    e.Graphics.FillRectangle(fillBrush, xPos, yPos, rectWidth, rectHeight);
+                }
+            }
+            outlinePen.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void folderSelectButton_Click(object sender, EventArgs e)
+        {
+           if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                currentSaveFolder = folderBrowserDialog1.SelectedPath;
+                Properties.Settings.Default.FolderPath = currentSaveFolder;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void mainTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            artRetiever.ConnectToAPI();
+        }
+        public void PrintString(string value)
+        {
+            mainTextBox.Text += value + Environment.NewLine;
+        }
+
+        private void retrieveButton_Click(object sender, EventArgs e)
+        {
+            if (currentSaveFolder == "")
+            {
+                PrintString("Folder not set.");
+                return;
+            }
+
+            artRetiever.GetAlbumArt(currentSaveFolder);
+            imageFiles = Directory.GetFiles(currentSaveFolder);
+
+
+            retrieveButton.Hide();
+            imageSelector.Visible = true;
+            imageSelector.Maximum = imageFiles.Length - 1;
+            
+            currentImage = Image.FromFile(imageFiles[0]);
+            pictureBox1.Image = currentImage;
+
+            DetectText();
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        public void HideSpotifyButton()
+        {
+            connectButton.Hide();
+          
+        }
+        public void ShowRetrieveButton()
+        {
+            retrieveButton.Visible = true;
+        }
+
+
+         void DetectText()
+        {
+            TesseractEngine tess = new TesseractEngine("C:\\Users\\Malcolm MacDonald\\Documents\\Visual Studio 2015\\Projects\\AlbumArt\\AlbumArt\\bin\\Debug\\tessdata", "eng");
+            
+            Page newPage = tess.Process(new Bitmap(currentImage),PageSegMode.AutoOsd);
+           
+            ResultIterator iterator = newPage.GetIterator();
+            string totalText = newPage.GetText();
+            currentRects = new Rect[totalText.Length];
+            for (int i = 0; i < totalText.Length; i++)
+            {
+                Rect foundRect = new Rect();
+                string symbolText = iterator.GetText(PageIteratorLevel.Symbol);
+                bool hasText = symbolText != "" && symbolText != null;
+                bool onlyLetters = false;
+                if (hasText)
+                {
+                    onlyLetters = symbolText.ToCharArray().All(s => char.IsLetter(s));
+                }
+
+                bool gotBoundingBox = iterator.TryGetBoundingBox(PageIteratorLevel.Symbol, out foundRect);
+                if (hasText && onlyLetters && gotBoundingBox)
+                {
+                    currentRects[i] = foundRect;
+                }
+                iterator.Next(PageIteratorLevel.Symbol);
+            }
+            pictureBox1.Refresh();
+            tess.Dispose();
+            iterator.Dispose();
+        }
+
+        private void imageSelector_ValueChanged(object sender, EventArgs e)
+        {
+            currentImage = Image.FromFile(imageFiles[(int)imageSelector.Value]);
+            pictureBox1.Image = currentImage;
+            DetectText();
+        }
+    }
+}
